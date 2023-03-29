6f54e56e7c0ccaa9f1ab51f356ebcaf764122f64
VezalisPetros <petrosvez@gmail.com>
2023-03-29 23:13:02 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+export default function Header() {
+    return (
+        <header className="header">
+           <img 
+                src="https://freepngimg.com/download/internet_meme/3-2-troll-face-meme-png.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            
+        </header>
+    )
+}
